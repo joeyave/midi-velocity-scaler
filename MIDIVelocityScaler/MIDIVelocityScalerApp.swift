b221873a436f6330e0edaf24002b6c1aeecee068
josephaveltsev <joseph.aveltsev@gmail.com>
2025-05-13 15:09:56 +0300

    Initial Commit

---
+++ MIDIVelocityScaler/MIDIVelocityScalerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDIVelocityScalerApp.swift
+//  MIDIVelocityScaler
+//
+//  Created by Joseph Aveltsev on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MIDIVelocityScalerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MIDIVelocityScalerTests/MIDIVelocityScalerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MIDIVelocityScalerTests.swift
+//  MIDIVelocityScalerTests
+//
+//  Created by Joseph Aveltsev on 13.05.2025.
+//
+
+import Testing
+@testable import MIDIVelocityScaler
+
+struct MIDIVelocityScalerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
